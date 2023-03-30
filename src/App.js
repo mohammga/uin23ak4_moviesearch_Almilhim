dabbcf83feab29416f85755e6774cd4bb4a5a9bc
Mohammed Ghazwan Almilhim <112077465+mohammga@users.noreply.github.com>
2023-03-30 08:49:50 +0200

    created routing for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import SearchResults from './pages/SearchResults';
+import AboutMovie from './pages/AboutMovie';
 import './css/main.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route index element={<SearchResults />} />
+                <Route path='/about-movie/:id' element={<AboutMovie />} />
+            </Route>
+        </Routes>
+    );
 }
-
 export default App;
